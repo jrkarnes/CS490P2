9073ad3a6aebd8cc803c8e336d9f1629e22c0518
cant4f <cant4f@mail.umkc.edu>
2015-11-04 18:22:45 -0600

    create.sql
    having trouble and cannot connect to the database. perhaps it is my home
    internet. I will keep at it, but wanted to post what i have for now so
    that you guys could see variable names.

---
+++ create.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Author(
+ID varchar(10),
+AuthorName VARCHAR(250) NOT NULL,
+Unique(ID),
+PRIMARY KEY(ID)
+);
+
+CREATE TABLE Book(
+ISBN varchar(15),
+Title varchar(250),
+Edition varchar(10),
+YearIssued int not null,
+PRIMARY KEY(ISBN)
+);
+
+CREATE TABLE Copy(
+barCode varchar(10),
+Book_ID varchar(15),
+PRIMARY KEY(barCode),
+FOREIGN KEY(Book_ID) REFERENCES Book(ISBN)
+);
+
+CREATE TABLE Authorship(
+ID int not null AUTO_INCREMENT,
+Book_ID varchar(15),
+Author_ID varchar(10)
+Main boolean,
+PRIMARY KEY(ID),
+FOREIGN KEY(Book_ID) REFERENCES Book(ISBN),
+FOREIGN KEY(Author_ID) REFERENCES Author(ID)
+);
+
+CREATE TABLE Member(
+ID int not null auto_increment,
+MemberName varchar(250),
+Gender ENUM{'1','2'},
+email varchar(250),
+PRIMARY KEY(ID)
+);
+
+CREATE TABLE Loan(
+ID int not null auto_increment,
+copyID varchar(10),
+memberID int,
+borrowDate Date,
+returnDate Date,
+status ENUM{'1','2','3',},
+PRIMARY KEY(ID),
+FOREIGN KEY(copyID) REFERENCES Copy(barCode),
+FOREIGN KEY(memberID) REFERENCES Member(ID)
+);
