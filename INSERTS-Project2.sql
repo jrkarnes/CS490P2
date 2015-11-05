663f0cd533620a00e41cd72a24dbc7b9f2723938
M1K43L4 <cubon.mikaela@yahoo.com>
2015-11-05 09:11:01 -0600

    Table two Added
    Inserts finished for 2 out of 6

--- INSERTS-Project2.sql
+++ INSERTS-Project2.sql
@@ -1,4 +1,15 @@
 INSERT INTO Author(ID, FullName)
 VALUES
-('1', 'Soren Lauesen')
-('2' 'Robert Lafore');
+('1', 'Soren Lauesen'),
+('2', 'Robert Lafore'),
+('3', 'Adam Trachtenberg'),
+('4', 'David Sklar'),
+('5', 'Kevin Tatroe');
+
+INSERT INTO Book(ISBN, Title, Edition, YearIssued)
+VALUES
+('0672324539', 'Data Structures and Algorithms in Java', '2', '2002'),
+('0321181433', 'User Interface Design: A Software Engineering Perspective', '1', '2004'),
+('0201745704',' Software Requirements: Styles and Techniques', '2', '2002'),
+('144936375X', 'PHP Cookbook: Solutions & Examples for PHP Programmers', '3', '2014'),
+('1449392776', 'Programming PHP', '3', '2013');
